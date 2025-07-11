19d2184d4d0d298a42261101201f1a1f73a8e98d
ostap-sushko <sushkoostap@gmail.com>
2025-07-11 21:25:47 +0300

    trd

---
+++ js/test1.js
@@ -0,0 +1,22 @@
+const age = 15;
+console.log(age);
+const name = "Ostap";
+console.log(typeof name);
+const isStudent = true;
+console.log(typeof isStudent);
+const myString = "ти не ти коли голодний";
+console.log(typeof myString);
+const myNumber = 20;
+console.log(myNumber + 10);
+const myNull = null;
+console.log(myNull);
+const yourName = "Введіть своє ім’я";
+const userName = prompt(yourName);
+const messageUpperCase = userName.toUpperCase();
+console.log(userName);
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+const warning = "Увага! Дія може бути небезпечною."
+alert(warning)
+const youAgreee = confirm("Ви впевнені, що хочете продовжити?");
+console.log(youAgreee);
